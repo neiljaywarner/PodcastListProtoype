caa34bc974b411399fde183a6779d74e6b435b85
Neil Jay Warner <njwandroid@gmail.com>
2017-10-17 09:12:22 -0500

    compiles using enqueue to log title

--- app/src/main/java/com/spiritflightapps/podcastlistprototype/MainActivity.kt
+++ app/src/main/java/com/spiritflightapps/podcastlistprototype/MainActivity.kt
@@ -3,6 +3,7 @@ package com.spiritflightapps.podcastlistprototype
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.spiritflightapps.podcastlistprototype.model.ArticleResponse
 import com.spiritflightapps.podcastlistprototype.network.ITunesRSSFeed
 import kotlinx.android.synthetic.main.activity_main.*
@@ -46,10 +47,25 @@ class MainActivity : AppCompatActivity() {
         val service = retrofit.create(ITunesRSSFeed::class.java)
 
         val feedCall = service.getFeed()
-        https://github.com/ikocijan/Kotlin-Dagger2-Retrofit2-example/blob/master/app/src/main/kotlin/xyz/ivankocijan/kotlinexample/mvp/interactor/impl/PokemonListInteractorImpl.kt
-
-feedCall.enqueue(object : Callback<ArticleResponse> {
+        // https://github.com/ikocijan/Kotlin-Dagger2-Retrofit2-example/blob/master/app/src/main/kotlin/xyz/ivankocijan/kotlinexample/mvp/interactor/impl/PokemonListInteractorImpl.kt
+        feedCall.enqueue(object : Callback<ArticleResponse> {
             override fun onResponse(call: Call<ArticleResponse>?, response: Response<ArticleResponse>?) {
-        }
-                if (response != null && response.isSuccessful) {    }
-                    pokemonListListener.onSuccess(response.body())
+                if (response != null && response.isSuccessful) {
+                    //pokemonListListener.onSuccess(response.body())
+                    val articleResponse : ArticleResponse? = response.body()
+                    Log.e("NJW", articleResponse?.channel?.title)
+                } else {
+                    // pokemonListListener.onFailure(appContext.getString(R.string.error_fetching_data))
+
+                }
+
+            }
+
+            override fun onFailure(call: Call<ArticleResponse>?, t: Throwable?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        })
+    }
+}
+
